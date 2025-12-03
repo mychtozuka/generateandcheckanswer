73fc354c475bcd741642b8e8851892d2547bb700
Kaze <tozukakazetaka@gmail.com>
2025-12-03 23:55:50 +0900

    Add Basic Authentication

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get('authorization');
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1];
+    const [user, pwd] = atob(authValue).split(':');
+
+    if (user === 'admin' && pwd === '12345') {
+      return NextResponse.next();
+    }
+  }
+
+  return new NextResponse('Authentication Required', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  });
+}
+
+export const config = {
+  matcher: '/:path*',
+};
